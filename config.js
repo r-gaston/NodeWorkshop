867030836be26e5b528173b7fde4116c91089f91
r-gaston <rgaston001@columbus.rr.com>
2022-01-07 15:29:52 -0500

    Passport Facebook

--- config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    'secretKey': '12345-67890-09876-54321',
-    'mongoUrl': 'mongodb://localhost:27017/nucampsite',
-    'facebook': {
-        clientId: '402020058389657',
-        clientSecret: 'b8aa5661981720eb2906f8e92fde066b'
-    }
-}
