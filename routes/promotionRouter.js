e2e8f4324bb8618b9e1c81ede379c88f7a1f7ac9
r-gaston <rgaston001@columbus.rr.com>
2022-01-07 03:57:40 -0500

    CORS

--- routes/promotionRouter.js
+++ routes/promotionRouter.js
@@ -51,7 +51,7 @@ promotionRouter.route('/:promotionId')
     })
     .catch(err => next(err));
 })
-.post(cors.corsWithOptions, authenticate.verifyUser, authenticate.verifyAdmin (req, res) => {
+.post(cors.corsWithOptions, authenticate.verifyUser, authenticate.verifyAdmin, (req, res, next) => {
     res.statusCode = 403;
     res.end(`POST operation not supported on /promotions/${req.params.promotionId}`);
 })
